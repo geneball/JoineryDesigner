46dd068eaa4c21f7d0637ad269f165b271566c47
Gene Ball <geneball@gmail.com>
2023-08-12 22:27:12 -0600

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -29,11 +29,14 @@ const ctx = canvas.getContext("2d")
 ctx.lineWidth = 1
 
 const WD = 500, HT = 500
+const sN = 8, sE = 4, sS = 2, sW = 1
+const xStp = [ 1, 1, 0, -1, -1, -1, 0, 1 ]
+const yStp = [ 0, 1, 1, 1, 0, -1, -1, -1 ]
+
+var onEdge = { N: false, E: false, S: false, W: false }
 var eW = 20
 var xW = WD - 2 * eW, yH = HT - 2 * eW
-var sL = xW > yH? xW / 12 : yH / 12
-var xStp = [  sL, sL, 0, -sL, -sL, 0, sL ]
-var yStp = [  0, sL, sL, sL, 0, -sL, -sL, -sL ]
+
 
 function addButtons( obj, fn ){
   for (let nm in obj ){
@@ -59,21 +62,46 @@ function clear(){
   ctx.fillRect( eW+xW,eW,  eW,yH ) // right
   ctx.fillRect( eW,eW+yH,  xW,eW ) // bottom
 }
-function showGrid( mat ){
-  let rws = mat.length, cls = mat[0].length
-  xW = WD / cls
-  yH = HT / rws
-  sL = xW > yH? xW / 12 : yH / 12
-  xStp = [  sL, sL, 0, -sL, -sL, -sL, 0, sL ]
-  yStp = [  0, sL, sL, sL, 0, -sL, -sL, -sL ]
+function showGrid( gr ){
+  let rows = gr.length, cols = gr[0].length
+  xW = WD / cols
+  yH = HT / rows
+  mL = yH > xW? xW : yH
+  sL = mL / 12
+  
+  let xTL = 0, yTL = 0
+  let vertRw = true
+  for( let r=0; r<rows; r++ ){
+    let vert = vertRw
+    vertRw = !vertRw
+    let n = r==0? edge : gr[r-1][c]
+    let s = r==rows-1? edge : gr[r+1][c]
+    for ( let c=0; c<cols; c++ ){
+      let ctr = gr[r][c]
+      let w = c==0? edge : gr[r][c-1]
+      let e = c==cols-1? edge : gr[r][c+1]
+      
+      if (!vert){
+        draw( xTL+mL,yTL,    0, n.BRtoBL )
+        draw( xTL+mL,yTL,    2, ctr.BLtoTL ) 
+        draw( xTL,   yTL+mL, 4, s.TLtoTR )
+        draw( xTL,yTL+mL,    6, ctr.BLtoTL )  
+      } else {
+        draw( xTL,   yTL,    0, ctr.TLtoTR )
+        draw( xTL+mL,yTL+mL, 2, e.BLtoTL ) 
+        draw( xTL+mL,yTL+mL, 4, ctr.BRtoBL )
+        draw( xTL,yTL+mL,    6, w.TRtoBR )
+      }
+      vert = !vert
+    }
+    vertRw =!vertRw
+  }
 }
 function showSquare( s ){
   xW = WD - 2 * eW
   yH = HT - 2 * eW
-  sL = xW > yH? xW / 12 : yH / 12
-  xStp = [  sL, sL, 0, -sL, -sL, 0, sL ]
-  yStp = [  0, sL, sL, sL, 0, -sL, -sL, -sL ]
-
+  mL = yH > xW? xW : yH
+  sL = mL / 12
   
   draw( eW,eW,             0, p )
   draw( eW+xW, eW,         2, p )
@@ -83,23 +111,24 @@ function showSquare( s ){
 function draw( x,y, dir, path ){
   ctx.beginPath();
   ctx.moveTo( x,y );
-
+  let f = 0
   for( let m of Array(path)){
     switch (m){
      case 'r': dir += 1; break
      case 'l': dir += 1; break
+     case 'R': dir += 1; break
+     case 'L': dir += 1; break 
 
-     case '1': 
-     case '2': 
-     case '3': 
-     case '4': 
-     case '5': 
-     case '6': 
-       x += xSt[dir] * m
-       y += ySt[dir] * m
-       break
+     case '1': f = 1; break 
+     case '2': f = 2; break 
+     case '3':  f = 3; break
+     case '4':  f = 4; break
+     case '5':  f = 5; break
+     case '6':  f = 6; break
+     case 'm': f = 12; break
      }
-     
+     x += xStep[dir] * f * sL
+     y += yStep[dir] * f * sL 
      ctx.lineTo( x,y );
   }
   ctx.closePath();
